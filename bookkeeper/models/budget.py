051e9483c4800575dd02ebef7bb7d1fe29994c2e
Victoriia00 <55548301+Victoriia00@users.noreply.github.com>
2023-03-13 22:22:03 +0300

    1

--- bookkeeper/models/budget.py
+++ bookkeeper/models/budget.py
@@ -2,12 +2,6 @@ from dataclasses import dataclass
 
 @dataclass(slots=True)
 class Budget:
-    """
-    amount - сумма
-    category - id категории расходов
-    time - промежуток времени в днях
-    pk - id записи в базе данных
-    """
     amount: int
     category: int
     time: int
